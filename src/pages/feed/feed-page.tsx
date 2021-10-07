a075b3d18228b36f134979aabbe22e748c03c09d
Matvei Bohush <bohush.mathew@gmail.com>
2021-10-07 15:00:02 +0300

    add preloader to feed

--- src/pages/feed/feed-page.tsx
+++ src/pages/feed/feed-page.tsx
@@ -3,48 +3,58 @@ import React, { useEffect, useState } from 'react'
 import { NavLink } from 'react-router-dom'
 import { Image } from '../../components/image/image'
 import { fetchAllImages } from '../../core/firebase/images-api'
+import { Preloader } from '../../utils/preloader/preloader'
 import { useStyles } from './feed-page.styles'
 
 export const FeedPage: React.FC = React.memo(() => {
 	const styles = useStyles()
 	const [images, setImages] = useState<any>()
+	const [isLoading, setIsLoading] = useState(false)
 
 	useEffect(() => {
+		setIsLoading(true)
 		fetchAllImages().then((res) => setImages(res))
+		setIsLoading(false)
 	}, [])
 
 	console.log(images)
 	return (
-		<Box className={styles.feed}>
-			<Box className={styles.buttons}>
-				<Button
-					variant="contained"
-					color="secondary"
-					className={styles.button}
-				>
-					<NavLink className={styles.link} to="/editor">
-						Editor
-					</NavLink>
-				</Button>
-				<Button
-					variant="contained"
-					color="secondary"
-					className={styles.button}
-				>
-					<NavLink className={styles.link} to="/">
-						Feed
-					</NavLink>
-				</Button>
-			</Box>
-			<Box>
-				{images ? (
-					images.map((image: any) => (
-						<Image image={image} key={image.imageId} />
-					))
-				) : (
-					<h1>No images</h1>
-				)}
-			</Box>
+		<Box>
+			{isLoading ? (
+				<Preloader />
+			) : (
+				<Box className={styles.feed}>
+					<Box className={styles.buttons}>
+						<Button
+							variant="contained"
+							color="secondary"
+							className={styles.button}
+						>
+							<NavLink className={styles.link} to="/editor">
+								Editor
+							</NavLink>
+						</Button>
+						<Button
+							variant="contained"
+							color="secondary"
+							className={styles.button}
+						>
+							<NavLink className={styles.link} to="/">
+								Feed
+							</NavLink>
+						</Button>
+					</Box>
+					<Box>
+						{images ? (
+							images.map((image: any) => (
+								<Image image={image} key={image.imageId} />
+							))
+						) : (
+							<h1>No images</h1>
+						)}
+					</Box>
+				</Box>
+			)}
 		</Box>
 	)
 })
