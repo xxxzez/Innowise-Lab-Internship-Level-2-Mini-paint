b3a539885f869e6128586989f1be02eb8275e88a
Matvei Bohush <bohush.mathew@gmail.com>
2021-10-11 15:41:28 +0300

    ref

--- src/app.styles.ts
+++ src/app.styles.ts
@@ -2,11 +2,6 @@ import { makeStyles } from '@material-ui/core'
 
 export const useStyles = makeStyles({
 	main: {
-		display: 'flex',
-		flexDirection: 'column',
-		justifyContent: 'center',
-		alignItems: 'center',
-		marginTop: '50px',
-		marginBottom: '40px',
+		margin: '40px 0',
 	},
 })
--- src/core/types/user-types.ts
+++ src/core/types/user-types.ts
@@ -1,7 +1,7 @@
 export type UserType = {
     email: string | null
     uid: string | null
-    photo: string | null
+    photo: string | null | undefined
 }  | null
 
 export type UserStateType = UserType | null
